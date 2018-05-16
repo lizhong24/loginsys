c0971dfe68268ec28cb04f30ee6f7adeab8ce328
lizhong24 <1979158178@qq.com>
2018-05-16 19:49:12 +0800

    登录及验证

--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from . import models
 
 # Create your views here.
 
@@ -10,7 +11,24 @@ def index(request):
 
 
 def login(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        message = "所有字段都必须填写！"
+        if username and password:  # 确保用户名和密码都不为空
+            username = username.strip()
+            # 用户名字符合法性验证
+            # 密码长度验证
+            # 更多的其它验证.....
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    return redirect('/index/')
+                else:
+                    message = "密码不正确！"
+            except:
+                message = "用户名不存在！"
+        return render(request, 'login/login.html', {"message": message})
     return render(request, 'login/login.html')
 
 
