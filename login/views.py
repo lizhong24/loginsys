db2e4057d441f82188061a37bf7603ba8f701384
lizhong24 <1979158178@qq.com>
2018-05-17 09:30:52 +0800

    表单模型使用

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from . import models
+from . import forms
 
 # Create your views here.
 
@@ -12,14 +13,11 @@ def index(request):
 
 def login(request):
     if request.method == "POST":
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+        login_form = forms.UserForm(request.POST)
         message = "所有字段都必须填写！"
-        if username and password:  # 确保用户名和密码都不为空
-            username = username.strip()
-            # 用户名字符合法性验证
-            # 密码长度验证
-            # 更多的其它验证.....
+        if login_form.is_valid():  # 确保用户名和密码都不为空
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
             try:
                 user = models.User.objects.get(name=username)
                 if user.password == password:
@@ -27,9 +25,10 @@ def login(request):
                 else:
                     message = "密码不正确！"
             except:
-                message = "用户名不存在！"
-        return render(request, 'login/login.html', {"message": message})
-    return render(request, 'login/login.html')
+                message = "用户不存在！"
+        return render(request, 'login/login.html', locals())
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', locals())
 
 
 def register(request):
